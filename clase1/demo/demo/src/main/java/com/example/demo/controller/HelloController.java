8d4c6d9168670c28629b6eeb2cd44d56ec5b6f01
Mancaceresuade <mancaceres@uade.edu.ar>
2026-03-09 11:20:09 -0300

    Demo crud api

--- clase1/demo/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ clase1/demo/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -18,8 +21,24 @@ public class HelloController {
         return nombres;
     }
 
+    @PostMapping("/nombres")
+    public String addNombre(@RequestParam String nombre) {
+        nombres.add(nombre);
+        return "Nombre agregado: " + nombre;
+    }
+
+    @DeleteMapping("/nombres")
+    public String deleteNombre(@RequestParam String nombre) {
+        if (nombres.remove(nombre)) {
+            return "Nombre eliminado: " + nombre;
+        } else {
+            return "Nombre no encontrado: " + nombre;
+        }
+    }
+
     @GetMapping("/hola")
-    public String hola() {
-        return "hola mundo";
+    public String hola(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        return "hola " + name;
     }
 }
